1e635749eb04abadfbb07cdc285660fc08311aad
RKoleda <R.Koleda@gmx.de>
2025-08-13 16:03:28 +0200

    Range Repetition added

---
+++ range_challenge.py
@@ -0,0 +1,19 @@
+for i in range(1, 6):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(3, 15, 3):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(2, 17, 2):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(22, 12, -2):
+    print(i, end=" ")
+print("\n---2")
+    
+for i in range(-5, 6):
+    print(i, end=" ")
+print("\n---")
