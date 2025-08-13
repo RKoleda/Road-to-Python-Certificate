e227b6b58d100d180a820a5480ff40427ab9485c
RKoleda <R.Koleda@gmx.de>
2025-08-13 15:09:37 +0200

    Repeating the basics

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hallo Python")
+
+name = "Rudi"
+alter = 34
+print(name, alter)
+
+summe = 5 + 3
+print("Summe:", summe)
+
+benutzername = input("Wie heisst du?")
+print("Schoen dich kennenzulernen", benutzername)
+
---
+++ notenrechner.py
@@ -0,0 +1,13 @@
+punkte = int(input("Punkte eingeben (0-100):"))
+
+if punkte >= 90:
+    print("Note: 1")
+elif punkte >= 80:
+    print("Note: 2")
+elif punkte >= 70:
+    print("Note: 3")
+elif punkte >= 60:
+    print("Note: 4")
+else:
+    print("Note 5")
+    
