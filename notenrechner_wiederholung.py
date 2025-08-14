86e6915a32d88395776ce130a7dc4aa7d3f1a5fa
RKoleda <R.Koleda@gmx.de>
2025-08-14 17:01:25 +0200

    Notenrechner Wiederholung zum aufwaermen

---
+++ notenrechner_wiederholung.py
@@ -0,0 +1,11 @@
+punkte = int(input("Hier bitte Punktzahl eingeben: "))
+
+if punkte >= 90:
+    print("Note 1")
+elif punkte >= 80:
+    print("Note 2")
+elif punkte >= 70:
+    print("Note 3")
+elif punkte < 70:
+    print("Durchgefallen")
+    
