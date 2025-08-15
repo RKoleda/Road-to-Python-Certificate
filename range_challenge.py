3e327d151f01c971f8d9652e6c42741a8ccb7c6e
RKoleda <R.Koleda@gmx.de>
2025-08-15 22:17:11 +0200

    range erweitert

--- range_challenge.py
+++ range_challenge.py
@@ -17,3 +17,44 @@ print("\n---2")
 for i in range(-5, 6):
     print(i, end=" ")
 print("\n---")
+
+# Zusatz 10 Stück:
+for i in range(10):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(5, 51, 5):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(100, 90, -1):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(0, 21, 4):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(-10, 0):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(50, 60):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(1, 102, 10):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(20, 0, -5):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(0, 101, 20):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(-100, -90):
+    print(i, end=" ")
+print("\n---")
