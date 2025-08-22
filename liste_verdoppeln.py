05e84bab37f12933330cfb21d06aa9210249c627
RKoleda <R.Koleda@gmx.de>
2025-08-22 22:42:33 +0200

    Paar Modifikationen vorgenommen

--- liste_verdoppeln.py
+++ liste_verdoppeln.py
@@ -4,3 +4,62 @@ neu = [z * 2 for z in zahlen]
 
 print(zahlen)
 print(neu)
+
+
+
+for i in range(1, 6):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(3, 15, 3):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(2, 17, 2):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(22, 12, -2):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(-5, 6):
+    print(i, end=" ")
+print("\n---")
+
+# Neue Beispiele
+
+for i in range(10):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(50, 61):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(100, 201, 25):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(-20, -10):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(5, 0, -1):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(0, 101, 10):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(7, 70, 7):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(1, 32, 3):
+    print(i, end=" ")
+print("\n---")
+
+for i in range(1000, 950, -5):
+    print(i, end=" ")
